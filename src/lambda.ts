f740ec885480e36054eab025bf359d570af19fea
Natan Santana <natan.santana.dev@gmail.com>
2025-08-20 23:32:24 -0300

    fix: adição do tratamento de binário no serveless do lambda

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,4 +1,20 @@
 import serverlessExpress from "@codegenie/serverless-express";
 import app from "./index";
 
-export const handler = serverlessExpress({ app });
+const binaryContentTypes: string[] = [
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+  'image/webp',
+  'application/octet-stream',
+  'application/pdf',
+  'application/zip',
+  'application/x-sqlite3',
+];
+
+export const handler = serverlessExpress({
+  app,
+  binarySettings: {
+    contentTypes: binaryContentTypes,
+  }
+});
