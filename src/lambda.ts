3c095dc778507d18ae9e42f93607f9f5d36695d2
Natan Santana <natan.santana.dev@gmail.com>
2025-08-20 23:18:52 -0300

    fix: remoção de configuração de binário do handler

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,20 +1,4 @@
 import serverlessExpress from "@codegenie/serverless-express";
 import app from "./index";
 
-const binaryContentTypes: string[] = [
-  'image/jpeg',
-  'image/png',
-  'image/gif',
-  'image/webp',
-  'application/octet-stream',
-  'application/pdf',
-  'application/zip',
-  'application/x-sqlite3',
-];
-
-export const handler = serverlessExpress({
-  app,
-  binarySettings: {
-    contentTypes: binaryContentTypes,
-  }
-});
+export const handler = serverlessExpress({ app });
