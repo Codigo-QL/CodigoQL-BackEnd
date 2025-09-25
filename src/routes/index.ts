cbc97e0e4eb088211d971a6f4d1647a124f8eb7e
Natan Santana <natan.santana.dev@gmail.com>
2025-09-25 09:10:49 -0300

    feat: adição da rota de login e sincronização do progresso local

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import nivelRouter from "./nivel.routes";
 import capituloRouter from "./capitulo.routes";
 import personagemRouter from "./personagem.routes";
+import alunoRouter from './aluno.routes';
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.use("/niveis", nivelRouter);
 router.use("/capitulos", capituloRouter);
 
 router.use("/personagens", personagemRouter);
+
+router.use("/alunos", alunoRouter);
 
 export default router;
